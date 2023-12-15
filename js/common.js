d2fbba828327852cea95644b3569dd7201316e58
Oscar Otero <oom@oscarotero.com>
2023-12-15 18:11:24 +0100

    axustes

--- js/common.js
+++ js/common.js
@@ -1,1 +1,17 @@
 import "./components/carousel.js";
+
+// Menu
+document.body.addEventListener("click", () => {
+  document.body.classList.remove("has-menu-open");
+});
+
+document.getElementById("menu-button").addEventListener("click", (ev) => {
+  document.body.classList.toggle("has-menu-open");
+  ev.stopPropagation();
+});
+document.getElementById("menu").addEventListener("click", (ev) => {
+  ev.stopPropagation();
+  setTimeout(() => {
+    document.body.classList.remove("has-menu-open");
+  }, 100);
+});
